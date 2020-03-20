69172fb57df691627c286a30198fbbd886f8f242
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-19 20:32:36 -0700

    psuedo code plus added in day and time

---
+++ script.js
@@ -0,0 +1,31 @@
+var hoursData = {
+  '9': '',
+  '10': '',
+  '11': '',
+  '12': '',
+  '13': '',
+  '14': '',
+  '15': '',
+  '16': '',
+  '17': ''
+};
+
+//display current day and time on screen
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+// let currentHour be the hour of the current time of day
+var currentHour = parseInt(moment().format("H"));
+
+var timeBlockElements = $(".time-block");
+// for each hour
+
+// get the hour of the block
+// get reference to all the hours
+// how to determine if time is future/present/past
+//create user object from submission with onclick button
+
+//store that data by settin it to local storage
+
+//have user input saved within local storage in order to retrieve whats on display
+//var timeBlocks = $(".time-block")
+
